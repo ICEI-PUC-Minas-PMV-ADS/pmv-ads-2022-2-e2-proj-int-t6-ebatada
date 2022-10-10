be9d19996f6fbd60fab75c6e0918743a84029b22
João Lucas <joaolucaspinheirotorres@gmail.com>
2022-10-10 20:20:37 -0300

    atualizacoes

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,26 +1,84 @@
 class NavBar {
-  constructor(paiSubNav, subNav, filhoSubNav) {
+  constructor(paiSubNav, subNav, filhoSubNav, setaSubNav) {
     this.paiSubNav = document.querySelector(paiSubNav);
     this.subNav = document.querySelector(subNav);
     this.navBar = document.querySelector(".left-navbar");
     this.filhoSubNav = document.querySelector(filhoSubNav);
     this.botaoSanduiche = document.querySelector(".nav-logo")
-
+    this.setaSubNav = document.querySelector(setaSubNav);
+    this.setaMinhaLoja = document.querySelector(".seta-minhaloja");
+    this.setaRelatorios = document.querySelector(".seta-relatorios");
+    this.setaConfiguracoes = document.querySelector(".seta-configuracoes");
+    this.minhaLoja = document.querySelector(".subnav-minhaloja");
+    this.relatorios = document.querySelector(".subnav-relatorios");
+    this.configuracoes = document.querySelector(".subnav-configuracoes");
+  }
+  abrirNavBar() {
+    this.navBar.classList.add('active');
+    this.ativasSetas();
+    this.ativarPaginaAtual();
+    this.setaAberta();
+  }
+  fecharNavBar() {
+    this.navBar.classList.remove('active');
+    this.desativarSetas();
+    this.fecharSubNavManual();
   }
   clicarBotaoSanduiche() {
     this.botaoSanduiche.addEventListener("click", () => {
-      if (this.navBar.classList.contains('active') == true) {
-        this.navBar.classList.remove('active');
-        this.subNav.classList.remove('active');
-
+      this.resetSeta();
+      if (this.navBar.classList.contains('active')) {
+        this.fecharNavBar();
       }
-      else if (this.navBar.classList.contains('active') == false) {
-        this.navBar.classList.add('active');
-        if (this.paiSubNav.classList.contains('pagina-atual') == true) {
-          this.subNav.classList.add('active');
-        }
-      }
+      else
+        this.abrirNavBar();
     });
+  }
+  abrirSubNav() {
+    if (this.navBar.classList.contains('active') == true)
+      this.subNav.classList.add('active');
+    else {
+      this.navBar.classList.add('active');
+      this.subNav.classList.add('active');
+      this.ativasSetas();
+    }
+  }
+  fecharSubNav() {
+    this.subNav.classList.remove('active');
+  }
+  fecharSubNavManual() {
+    this.minhaLoja.classList.remove('active');
+    this.relatorios.classList.remove('active');
+    this.configuracoes.classList.remove('active');
+  }
+  ativarPaginaAtual() {
+    if (this.paiSubNav.classList.contains('pagina-atual') == true)
+      this.subNav.classList.add('active');
+  }
+  ativasSetas() {
+    this.setaMinhaLoja.classList.add('active')
+    this.setaRelatorios.classList.add('active')
+    this.setaConfiguracoes.classList.add('active')
+  }
+  desativarSetas() {
+    this.setaMinhaLoja.classList.remove('active')
+    this.setaRelatorios.classList.remove('active')
+    this.setaConfiguracoes.classList.remove('active')
+  }
+  setaAberta() {
+    if (this.paiSubNav.classList.contains('minha-loja') == true) this.setaMinhaLoja.classList.add('open');
+    if (this.paiSubNav.classList.contains('relatorios') == true) this.setaRelatorios.classList.add('open');
+    if (this.paiSubNav.classList.contains('configuracoes') == true) this.setaConfiguracoes.classList.add('open');
+  }
+  setaFechada() {
+    if (this.paiSubNav.classList.contains('minha-loja') == true) this.setaMinhaLoja.classList.remove('open');
+    if (this.paiSubNav.classList.contains('relatorios') == true) this.setaRelatorios.classList.remove('open');
+    if (this.paiSubNav.classList.contains('configuracoes') == true) this.setaConfiguracoes.classList.remove('open');
+  }
+  resetSeta() {
+    this.setaMinhaLoja.classList.remove('open');
+    this.setaRelatorios.classList.remove('open');
+    this.setaConfiguracoes.classList.remove('open');
   }
   configurarPaginaSimples() {
     if (this.paiSubNav.classList.contains('caixa') && document.title == "Caixa - éBatata"
@@ -29,9 +87,7 @@ class NavBar {
       this.paiSubNav.classList.toggle('pagina-atual');
       this.clicarBotaoSanduiche();
     }
-
   }
-
   configurarPaginaAtual() {
     if (this.paiSubNav.classList.contains('minha-loja') == true && this.filhoSubNav.classList.contains('pagina-informacoes') == true && document.title == "Minha Loja - éBatata"
       || this.paiSubNav.classList.contains('minha-loja') == true && this.filhoSubNav.classList.contains('pagina-produtos') == true && document.title == "Produtos - éBatata"
@@ -53,47 +109,21 @@ class NavBar {
       this.filhoSubNav.classList.remove('pagina-atual');
     }
   }
-
   clicarSubNav() {
     this.paiSubNav.addEventListener("click", () => {
       if (this.subNav.classList.contains('active') == true) {
-        this.subNav.classList.remove('active');
+        this.fecharSubNav();
+        this.setaFechada();
       }
-      else if (this.navBar.classList.contains('active') == true) {
-        this.subNav.classList.add('active');
+      else {
+        this.fecharSubNavManual();
+        this.resetSeta();
+        this.abrirSubNav();
+        this.setaAberta();
       }
-      else if (this.navBar.classList.contains('active') == false) {
-        this.navBar.classList.add('active');
-        this.subNav.classList.add('active');
-      }
-      this.botaoSanduiche.addEventListener("click", () => {
-        this.subNav.classList.remove('active');
-      })
-
-
     });
   }
 };
-/*const setaMinhaLoja = document.querySelector(".seta-minhaloja");
-const setaRelatorios = document.querySelector(".seta-relatorios");
-const setaConfiguracoes = document.querySelector(".seta-configuracoes");
-const botaoSanduiche = document.querySelector(".nav-logo");
-
-botaoSanduiche.addEventListener("click", () => {
-  if (setaMinhaLoja.classList.contains('active') == true
-    || setaRelatorios.classList.contains('active') == true
-    || setaConfiguracoes.classList.contains('active') == true) {
-    setaMinhaLoja.classList.remove('active')
-    setaRelatorios.classList.remove('active')
-    setaConfiguracoes.classList.remove('active')
-  }
-  else {
-    setaMinhaLoja.classList.add('active')
-    setaRelatorios.classList.add('active')
-    setaConfiguracoes.classList.add('active')
-  }
-});
-*/
 
 const caixa = new NavBar(".caixa");
 caixa.configurarPaginaSimples();
@@ -105,10 +135,10 @@ const cardapioOnline = new NavBar(".cardapio-online");
 cardapioOnline.configurarPaginaSimples();
 
 const minhaLoja = new NavBar(".minha-loja", ".subnav-minhaloja");
-const informacoes = new NavBar(".minha-loja", ".subnav-minhaloja", ".pagina-informacoes");
-const produtos = new NavBar(".minha-loja", ".subnav-minhaloja", ".pagina-produtos");
-const clientes = new NavBar(".minha-loja", ".subnav-minhaloja", ".pagina-clientes");
-const taxas = new NavBar(".minha-loja", ".subnav-minhaloja", ".pagina-taxas");
+const informacoes = new NavBar(".minha-loja", ".subnav-minhaloja", ".pagina-informacoes", ".seta-minhaloja");
+const produtos = new NavBar(".minha-loja", ".subnav-minhaloja", ".pagina-produtos", ".seta-minhaloja");
+const clientes = new NavBar(".minha-loja", ".subnav-minhaloja", ".pagina-clientes", ".seta-minhaloja");
+const taxas = new NavBar(".minha-loja", ".subnav-minhaloja", ".pagina-taxas", ".seta-minhaloja");
 
 minhaLoja.clicarSubNav();
 informacoes.configurarPaginaAtual();
@@ -117,9 +147,9 @@ clientes.configurarPaginaAtual();
 taxas.configurarPaginaAtual();
 
 const relatorios = new NavBar(".relatorios", ".subnav-relatorios");
-const historicoCaixa = new NavBar(".relatorios", ".subnav-relatorios", ".pagina-historico-caixa");
-const historicoPedidos = new NavBar(".relatorios", ".subnav-relatorios", ".pagina-historico-pedidos");
-const produtosMaisVendidos = new NavBar(".relatorios", ".subnav-relatorios", ".pagina-mais-vendidos");
+const historicoCaixa = new NavBar(".relatorios", ".subnav-relatorios", ".pagina-historico-caixa", ".seta-relatorios");
+const historicoPedidos = new NavBar(".relatorios", ".subnav-relatorios", ".pagina-historico-pedidos", ".seta-relatorios");
+const produtosMaisVendidos = new NavBar(".relatorios", ".subnav-relatorios", ".pagina-mais-vendidos", ".seta-relatorios");
 
 relatorios.clicarSubNav();
 historicoCaixa.configurarPaginaAtual();
@@ -127,13 +157,12 @@ historicoPedidos.configurarPaginaAtual();
 produtosMaisVendidos.configurarPaginaAtual();
 
 const configuracoes = new NavBar(".configuracoes", ".subnav-configuracoes");
-const loginsAdministrativos = new NavBar(".configuracoes", ".subnav-configuracoes", ".pagina-logins");
-const impressoras = new NavBar(".configuracoes", ".subnav-configuracoes", ".pagina-impressoras");
+const loginsAdministrativos = new NavBar(".configuracoes", ".subnav-configuracoes", ".pagina-logins", ".seta-configuracoes");
+const impressoras = new NavBar(".configuracoes", ".subnav-configuracoes", ".pagina-impressoras", ".seta-configuracoes");
 
 configuracoes.clicarSubNav();
 loginsAdministrativos.configurarPaginaAtual();
 impressoras.configurarPaginaAtual();
-
 
 
 
