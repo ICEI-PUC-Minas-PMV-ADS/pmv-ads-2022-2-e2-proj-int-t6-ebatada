d85f563fbd33f797ab71765a7aba44ad88914d67
Eduardo <103695641+EdduLobo@users.noreply.github.com>
2022-11-25 01:13:15 -0300

    1.13

--- src/public/scripts/order-history.js
+++ src/public/scripts/order-history.js
@@ -1,26 +1,86 @@
 function updateOrders() {
-  fetch("http://localhost:5000/orders/historicoPedidos")
-    .then((res) => {
-      return res.json();
-    })
-    .then((json) => {
-      let elementsOrder = "";
-      let orders = JSON.parse(json);
-      orders.forEach((order) => {
-        let elementOrder = `<tr class="found-orders">
-      <td data-label="id">${order.id}</td>
-      <td data-label="emissao">${order.emissao}</td>
-      <td data-label="cliente">${order.cliente}</td>
-      <td data-label="telefone">${order.telefone}</td>
-      <td data-label="endereco">${order.endereco}</td>
-     <td data-label="status">${order.status}</td>
+  if (document.querySelector("#radiOne").checked) {
+    fetch("http://localhost:5000/api/pedidos")
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        let elementsOrder = "";
+        let orders = json;
+        orders.forEach((order) => {
+          let elementOrder = `<tr class="found-orders">
+      <td data-label="id">${order.idpedido}</td>
+      <td data-label="emissao">00/00/00</td>
+      <td data-label="cliente">${order.cliente_cliente.nome}</td>
+      <td data-label="telefone">${order.cliente_cliente.telefoneprimario}</td>
+      <td data-label="endereco">${order.cliente_cliente.rua}, ${order.cliente_cliente.numero},  ${order.cliente_cliente.bairro}</td>
+      <td data-label="status">---</td>
       <td data-label="pago">${order.pago}</td>
-      <td data-label="itens">${order.itens}</td>
-      <td data-label="desconto">${order.desconto}</td>
-      <td data-label="total_final">${order.total_final}</td>
+      <td data-label="total_final">0000</td>
   </tr>`;
-        elementsOrder += elementOrder;
+          elementsOrder += elementOrder;
+        });
+        document.querySelector("#registerListBody").innerHTML = elementsOrder;
       });
-      document.querySelector("#registerListBody").innerHTML = elementsOrder;
-    });
+  } else if (document.querySelector("#radioTwo").checked) {
+    var selectMes = document.querySelector("#select-mes");
+    var mes = selectMes.options[selectMes.selectedIndex].value;
+    var dia = parseInt(document.querySelector("#day-dia").value);
+    var selectAmes = document.querySelector("#select-a-mes");
+    var ames = selectAmes.options[selectAmes.selectedIndex].value;
+    var adia = parseInt(document.querySelector("#day-a-dia").value);
+    fetch(
+      "http://localhost:5000/api/pedidosdata?mes=" +
+        mes +
+        "&dia=" +
+        dia +
+        "&ames=" +
+        ames +
+        "&adia=" +
+        adia +
+        ""
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        let elementsOrder = "";
+        let orders = json;
+        orders.forEach((order) => {
+          let elementOrder = `<tr class="found-orders">
+       <td data-label="id">${order.idpedido}</td>
+       <td data-label="emissao">00/00/00</td>
+       <td data-label="cliente">${order.cliente_cliente.nome}</td>
+       <td data-label="telefone">${order.cliente_cliente.telefoneprimario}</td>
+       <td data-label="endereco">${order.cliente_cliente.rua}, ${order.cliente_cliente.numero}, ${order.cliente_cliente.bairro}</td>
+       <td data-label="status">---</td>
+       <td data-label="pago">${order.pago}</td>
+       <td data-label="total_final">0000</td>
+  </tr>`;
+          elementsOrder += elementOrder;
+        });
+        document.querySelector("#registerListBody").innerHTML = elementsOrder;
+      });
+  }
 }
+
+updateOrders();
+
+document.querySelector("#view").addEventListener("click", () => {
+  document.querySelector("#registerListBody").innerHTML = "";
+  updateOrders();
+});
+
+document.querySelector("#radioTwo").addEventListener("click", () => {
+  const container = document.querySelectorAll(".separator");
+  for (var i = 0; i < container.length; i++) {
+    container[i].classList.add("ativo");
+  }
+});
+
+document.querySelector("#radiOne").addEventListener("click", () => {
+  const container = document.querySelectorAll(".separator");
+  for (var i = 0; i < container.length; i++) {
+    container[i].classList.remove("ativo");
+  }
+});
