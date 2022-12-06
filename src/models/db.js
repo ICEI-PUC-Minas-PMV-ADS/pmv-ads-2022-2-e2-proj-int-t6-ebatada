d03876507ab32724d64cbb237510520434734342
Eduardo <103695641+EdduLobo@users.noreply.github.com>
2022-12-06 15:25:59 -0300

    DASDASDSA

--- src/models/db.js
+++ src/models/db.js
@@ -1,25 +1,27 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize({
   database: "eBatata",
   username: "ebatata",
   password: "eTeste321",
-  host: "ebatata.postgres.database.azure.com",
+  host: "ebatata2.postgres.database.azure.com",
   port: 5432,
   dialect: "postgres",
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false
-    }
+      rejectUnauthorized: false,
+    },
   },
 });
 
-sequelize.authenticate()
+sequelize
+  .authenticate()
   .then(() => {
-    console.log('Connection has been established successfully.');
-  }).catch((error) => {
-    console.log('Unable to connect to the database:', error);
+    console.log("Connection has been established successfully.");
   })
+  .catch((error) => {
+    console.log("Unable to connect to the database:", error);
+  });
 
 module.exports = sequelize;
